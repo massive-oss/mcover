d69b658e93891dfeb24b38a12a4f9808c6e204ed
Dominic De Lorenzo <dom@misprintt.net>
2012-10-25 16:50:32 +1100

    changed cache file naming, pushed 1.4.2 to haxelib

--- build/Build.hx
+++ build/Build.hx
@@ -11,7 +11,7 @@ class Build extends mtask.core.BuildBase
 	{
 		t.url = "http://github.com/massiveinteractive/mcover";
 		t.description = "A cross platform code coverage framework for Haxe with testing and profiling applications. Supports AVM1, AVM2, JavaScript, C++, PHP and Neko.";
-		t.versionDescription = "Minor bugfixes. See CHANGES for full list of changes.";
+		t.versionDescription = "Minor tweaks to support haxe svn (2.11). See CHANGES for details.";
 
 		t.addTag("cross");
 		t.addTag("macro");
